932cf12e90d1d4f9a5bedc3b6c2231492515d611
makebono <xujiant@sfu.ca>
2017-11-20 15:52:25 +0800

    Graham scan and Jarvis march finished. Deguging is funny.

--- src/com/makebono/algorithms/test/TestField.java
+++ src/com/makebono/algorithms/test/TestField.java
@@ -14,24 +14,23 @@ public class TestField {
     public static void main(final String[] args) {
         final GrahamScan<Integer> gs = new GrahamScan<Integer>();
 
-        gs.add(1, 1, 0, -4);
-        gs.add(2, 3, 1, -2);
-        gs.add(3, 3, -4, 2);
-        gs.add(4, 3, -3, 1);
-        gs.add(5, 3, -3, -2);
-        gs.add(6, 3, -3.9, -3.9);
+        gs.add(1, 1, 3, -3);
+        gs.add(2, 3, 0.5, 0.5);
+        gs.add(3, 3, 3, 0);
+        gs.add(4, 3, 0, 3);
+        gs.add(5, 3, -2, 1);
+        gs.add(6, 3, -1.5, -2);
 
         System.out.println(gs);
 
         final JarvisMarch<Integer> jm = new JarvisMarch<Integer>();
-        jm.add(1, 1, 0, -4);
-        jm.add(2, 3, 1, -2);
-        jm.add(3, 3, -4, 2);
-        jm.add(4, 3, -3, 1);
-        jm.add(5, 3, -3, -2);
-        jm.add(6, 3, -3.9, -3.9);
+        jm.add(1, 1, 3, -3);
+        jm.add(2, 3, 0.5, 0.5);
+        jm.add(3, 3, 3, 0);
+        jm.add(4, 3, 0, 3);
+        jm.add(5, 3, -2, 1);
+        jm.add(6, 3, -1.5, -2);
 
         System.out.println(jm);
-
     }
 }
